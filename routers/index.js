116ad3461dafab8f047920b1e888541866946328
Bayuaji64 <bayuajipradipta64@gmail.com>
2023-03-04 01:21:35 +0700

    feat:done test users kurang test patch image

--- routers/index.js
+++ routers/index.js
@@ -1,18 +1,22 @@
-const Controller = require("../controllers");
+
+const Controller = require("../controllers/index");
+const ControllerUpdate = require('../controllers/updateData')
 const checkUser = require("../middlewares/checkUser");
 
 const router = require("express").Router();
+
+
 
 router.post("/register", Controller.register);
 router.post("/login", Controller.login);
 router.get("/users", Controller.getUser);
 router.get("/users/:id", Controller.getUserId);
-router.patch("/image/:userId", checkUser, Controller.updateImage);
-router.patch("/addbalance/:userId", checkUser, Controller.addBalance);
-router.patch("/reducebalance/:userId", checkUser, Controller.reducedBalance);
+
+router.patch("/image/:userId",checkUser,ControllerUpdate.updateImage);
+
+router.patch("/addbalance/:userId",checkUser, ControllerUpdate.addBalance);
+router.patch("/reducebalance/:userId",checkUser, ControllerUpdate.reducedBalance);
 router.get(
-  "/histories/balance/:userId",
-  checkUser,
-  Controller.getHistoryBalance
+  "/histories/balance/:userId",checkUser,Controller.getHistoryBalance
 );
 module.exports = router;
