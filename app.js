3ce9aa0435c237b1feb582d85b1a5ff19d14535a
Mitra Surya <mitrasurya7@gmail.com>
2023-03-05 00:21:43 +0700

    feat: user

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-if (process.env.NODE_ENV != 'production') {
-    require('dotenv').config();
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const router = require('./routers');
+const router = require("./routers");
 const port = process.env.PORT || 4001;
-const cors = require('cors');
-const errMsg = require('./middlewares/errorHandle');
+const cors = require("cors");
+const errMsg = require("./middlewares/errorHandle");
 
 app.use(cors());
-app.get('/', (req, res) => {
-    res.send('Masuk');
+app.get("/", (req, res) => {
+  res.send("Masuk");
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +20,7 @@ app.use(router);
 app.use(errMsg);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
 
 // module.exports = app;
