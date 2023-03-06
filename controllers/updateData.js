8e6b64df3fb6b2bae40cd212f98521db549f7de3
mashayyik <mashayyik@gmail.com>
2023-03-06 16:27:20 +0700

    refactor: change status to in/out

--- controllers/updateData.js
+++ controllers/updateData.js
@@ -1,91 +1,84 @@
-const midTransaction = require("../helpers/midtrans");
-const { User, HistoryBalance } = require("../models");
+const midTransaction = require('../helpers/midtrans');
+const { User, HistoryBalance } = require('../models');
 
 class ControllerUpdate {
-  static async updateImage(req, res, next) {
-    try {
-      const { userId } = req.params;
-      const { imageProfile } = req.body;
-      const updateImage = await User.update(
-        { imageProfile },
-        { where: { id: userId } }
-      );
-      // if (!updateImage) throw { name: "notImage" };
-      if (!imageProfile) throw { name: "notImage" };
-      res.status(200).json({ message: "success update image" });
-    } catch (error) {
-      // console.log(error);
-      next(error);
+    static async updateImage(req, res, next) {
+        try {
+            const { userId } = req.params;
+            const { imageProfile } = req.body;
+            const updateImage = await User.update({ imageProfile }, { where: { id: userId } });
+            // if (!updateImage) throw { name: "notImage" };
+            if (!imageProfile) throw { name: 'notImage' };
+            res.status(200).json({ message: 'success update image' });
+        } catch (error) {
+            // console.log(error);
+            next(error);
+        }
     }
-  }
-  static async payment(req, res, next) {
-    try {
-      const { balance } = req.body;
-      const { userId } = req.params;
-      const data = await User.findByPk(userId);
-      const dataUser = {
-        ...data,
-      };
-      delete dataUser.password;
-      const Tokentrans = await midTransaction(dataUser, balance);
-      console.log(Tokentrans);
-      res.status(200).json(Tokentrans);
-    } catch (error) {
-      next(error);
+    static async payment(req, res, next) {
+        try {
+            const { balance } = req.body;
+            const { userId } = req.params;
+            const data = await User.findByPk(userId);
+            const dataUser = {
+                ...data,
+            };
+            delete dataUser.password;
+            const Tokentrans = await midTransaction(dataUser, balance);
+            console.log(Tokentrans);
+            res.status(200).json(Tokentrans);
+        } catch (error) {
+            next(error);
+        }
     }
-  }
-  static async addBalance(req, res, next) {
-    try {
-      const { userId } = req.params;
-      const { balance } = req.body;
-      const data = await User.findByPk(userId);
-      const dataUser = {
-        ...data,
-      };
-      delete dataUser.password;
-      const result = Number(data.balance) + Number(balance);
-      await User.update({ balance: result }, { where: { id: userId } });
-      await HistoryBalance.create({
-        UserId: userId,
-        initialBalance: data.balance,
-        transaction: balance,
-        status: "credit",
-      });
-      const finalBalance = await User.findByPk(userId);
-      res
-        .status(200)
-        .json({ message: `balance terUpdate ${finalBalance.balance}` });
-      // console.log(updateBalance);
-    } catch (error) {
-      // console.log(error);
+    static async addBalance(req, res, next) {
+        try {
+            const { userId } = req.params;
+            const { balance } = req.body;
+            const data = await User.findByPk(userId);
+            const dataUser = {
+                ...data,
+            };
+            delete dataUser.password;
+            const result = Number(data.balance) + Number(balance);
+            await User.update({ balance: result }, { where: { id: userId } });
+            await HistoryBalance.create({
+                UserId: userId,
+                initialBalance: data.balance,
+                transaction: balance,
+                status: 'in',
+            });
+            const finalBalance = await User.findByPk(userId);
+            res.status(200).json({ message: `balance terUpdate ${finalBalance.balance}` });
+            // console.log(updateBalance);
+        } catch (error) {
+            // console.log(error);
 
-      next(error);
+            next(error);
+        }
     }
-  }
 
-  static async reducedBalance(req, res, next) {
-    try {
-      const { userId } = req.params;
-      const { balance } = req.body;
-      const data = await User.findByPk(userId);
-      const result = Number(data.balance) - Number(balance);
-      if (result < 0) throw { name: "balance_0" };
-      await User.update({ balance: result }, { where: { id: userId } });
-      await HistoryBalance.create({
-        UserId: userId,
-        initialBalance: data.balance,
-        transaction: balance,
-        status: "debit",
-      });
-      const finalBalance = await User.findByPk(userId);
-      res
-        .status(200)
-        .json({ message: `balance terUpdate ${finalBalance.balance}` });
-      // console.log(updateBalance);
-    } catch (error) {
-      next(error);
+    static async reducedBalance(req, res, next) {
+        try {
+            const { userId } = req.params;
+            const { balance } = req.body;
+            const data = await User.findByPk(userId);
+            const result = Number(data.balance) - Number(balance);
+            if (result < 0) throw { name: 'balance_0' };
+            await User.update({ balance: result }, { where: { id: userId } });
+            await HistoryBalance.create({
+                UserId: userId,
+                initialBalance: data.balance,
+                transaction: balance,
+                status: 'out',
+            });
+            const finalBalance = await User.findByPk(userId);
+            res.status(200).json({ message: `balance terUpdate ${finalBalance.balance}` });
+            // console.log(updateBalance);
+        } catch (error) {
+            next(error);
+        }
     }
-  }
 }
 
 module.exports = ControllerUpdate;
